32a5a26c9689500c481d3a4b75ba75e02a878202
carmelolg <carmelolagamba@hotmail.it>
2020-12-17 12:26:18 +0100

    add get http for alarm

--- routers/alarm.js
+++ routers/alarm.js
@@ -8,6 +8,13 @@ dotenv.config();
 
 const api = apiAdapter(process.env.ALARM_URL)
 
+router.get('/alarm', (req, res) => {
+    api
+    .get(req.path, httpConfig(req))
+    .then(resp => res.send(resp.data))
+    .catch(err => res.send(err))
+})
+
 router.post('/alarm', (req, res) => {
     api
     .post(req.path, req.body, httpConfig(req))
